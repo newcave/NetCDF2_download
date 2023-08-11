3514883b76e6abfaf9d3c4f56270a884361a3f44
Sung Hoon KIM <92258672+newcave@users.noreply.github.com>
2023-08-11 11:01:17 +0900

    Create app.py

---
+++ app.py
@@ -0,0 +1,49 @@
+import streamlit as st
+import requests
+from datetime import datetime, timedelta
+
+# Streamlit configuration
+st.set_page_config(page_title="Data Downloader", layout="wide")
+
+def download_data(start_datetime, end_datetime):
+    base_url = "http://opendata.kwater.or.kr/pubdata/kppm/kppm1Hr.do"
+    date_format = "%Y%m%d%H"
+    time_interval = timedelta(hours=3)
+    
+    start_dt = datetime.strptime(start_datetime, date_format)
+    end_dt = datetime.strptime(end_datetime, date_format)
+
+    current_dt = start_dt
+    while current_dt <= end_dt:
+        formatted_dt = current_dt.strftime(date_format)
+        file_url = f"{base_url}?searchTm={formatted_dt}"
+        filename = f"RN_KPPM01_NetCDF_{formatted_dt}.NC"
+        
+        response = requests.get(file_url)
+        if response.status_code == 200:
+            with open(filename, "wb") as file:
+                file.write(response.content)
+            st.write(f"Downloaded: {filename}")
+        else:
+            st.write(f"Failed to download: {filename}")
+
+        current_dt += time_interval
+
+def main():
+    st.title("Data Downloader")
+    
+    start_date = st.date_input("Start Date")
+    start_time = st.time_input("Start Time")
+
+    end_date = st.date_input("End Date")
+    end_time = st.time_input("End Time")
+    
+    start_datetime = f"{start_date.strftime('%Y%m%d')}{start_time.strftime('%H')}"
+    end_datetime = f"{end_date.strftime('%Y%m%d')}{end_time.strftime('%H')}"
+    
+    if st.button("Download Data"):
+        download_data(start_datetime, end_datetime)
+        st.success("Download completed!")
+
+if __name__ == "__main__":
+    main()
